ea4c9c8c0535b13d923ac2f8f3baf587fd04dfd5
lifeandtimesofnoonoo <matt.goengland@gmail.com>
2017-02-15 15:13:45 +0000

    Tidied up + commented a bit

--- raspi-wall-crash.c
+++ raspi-wall-crash.c
@@ -3,16 +3,15 @@
 #include <wiringPi.h>
 #include <lcd.h>
 #include <math.h>
-#include <wiringSerial.h> 
+#include <wiringSerial.h>
 #include <errno.h>
- 
-#define TIME_OUT_VALUE 12000
-#define DISTANCE_AVERAGE_NUMBER 5
 
-#define TRIG 5
-#define ECHO 6
-       
- 
+#define TIME_OUT_VALUE 12000            //Number of microseconds to wait before timing out - sound travels 1m in ~3000 microseconds, must travel to and from object
+#define DISTANCE_AVERAGE_NUMBER 5       //Number of readings to take averages from
+
+#define TRIG 5          //Defining pin used for sending a pulse
+#define ECHO 6          //Defining pin used for listening for a pulse
+
 //USE WIRINGPI PIN NUMBERS
 #define LCD_RS  25               //Register select pin
 #define LCD_E   24               //Enable Pin
@@ -21,120 +20,110 @@
 #define LCD_D6  21               //Data pin 6
 #define LCD_D7  14               //Data pin 7
 
-  
-
- 
-
-
-void setup() {
-        if (wiringPiSetup () == -1)
-  {
-	  	printf("hello3\n");
-    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
-  }
-        pinMode(TRIG, OUTPUT);
-        pinMode(ECHO, INPUT);
- 
-        //TRIG pin must start LOW
-        digitalWrite(TRIG, LOW);
-        delay(30);
-}
- 
-float getCM()
+void setup()
 {
-	//Send trig pulse
+    if (wiringPiSetup () == -1)
+    {
+        fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+    }
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+
+    //TRIG pin must start LOW
+    digitalWrite(TRIG, LOW);
+    delay(30);
+}
+
+float getCM()       //Function to calculate the distance a single pulse has travelled in cm
+{
+    //Send trig pulse
     digitalWrite(TRIG, HIGH);
     delayMicroseconds(20);
     digitalWrite(TRIG, LOW);
- 
+
     //Wait for echo start
     long startTime = micros();
     while(digitalRead(ECHO) == LOW);
     {
-		int travelTime = micros() - startTime;
-		if(travelTime > TIME_OUT_VALUE)
-		{
-			return -1;
-		}
-	}
-    
+        int travelTime = micros() - startTime;
+        if(travelTime > TIME_OUT_VALUE)
+        {
+            return -1;
+        }
+    }
+
     long travelTime = micros() - startTime;
 
- 
     //Wait for echo end
     startTime = micros();
     while(digitalRead(ECHO) == HIGH)
     {
-		travelTime = micros() - startTime;
-		if(travelTime > TIME_OUT_VALUE)
-		{
-			return -1;
-		}
-	}
-    
+        travelTime = micros() - startTime;
+        if(travelTime > TIME_OUT_VALUE)
+        {
+            return -1;
+        }
+    }
+
     travelTime = micros() - startTime;
- 
+
     //Get distance in cm
-    float distance = (float)travelTime / 58;
-        
+    float distance = (float)travelTime / 58;        //Sound takes ~29 microseconds to travel 1 cm, so dividing here by 58 accounts for the sound wave to be sent out and then travel back
+
     return distance;
 }
 
 float averageDistance()
 {
-	int i;
-	
-	float distance, y;
-	
-	for(i = 0; i < DISTANCE_AVERAGE_NUMBER+1; i++)
-	{
-		do
-		{
-			y = getCM();
-		}while(y == -1);
-		
-		distance += y;
-	}
-	
-	return (float)distance / DISTANCE_AVERAGE_NUMBER;
+    int i;
+
+    float distance, unAveragedDistance;
+
+    for(i = 0; i < DISTANCE_AVERAGE_NUMBER+1; i++)
+    {
+        do
+        {
+            unAveragedDistance = getCM();
+        }
+        while(unAveragedDistance == -1);                //If unAveragedDistance is -1 then time out has occurred - take another reading
+
+        distance += unAveragedDistance;
+    }
+
+    return (float)distance / DISTANCE_AVERAGE_NUMBER;
 }
- 
+
 int main(void)
 {
-	setup();
-    
+    setup();
+
     int lcd, fd, count;
-	unsigned int nextTime ;
-	
-    
-    lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);          
-    lcdPuts(lcd, "Hello, world!"); 
+
+    lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+    lcdPuts(lcd, "Hello, world!");
     delay(1000);
     lcdClear(lcd);
-  
-	fd = serialOpen("/dev/ttyAMA0", 57600);
-	
-	if (fd < 0)
-	{
-	  	printf("hello1\n");
-		fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-		return 1 ;
-	}
 
-	nextTime = millis () + 300 ;
+    fd = serialOpen("/dev/ttyAMA0", 57600);
 
-	float distance;
-	
+    if (fd < 0)
+    {
+        fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+        return 1 ;
+    }
+
+    float distance;
+
     do
     {
-		serialPrintf(fd, "#Bbff040,040");
-		distance = averageDistance();
-		lcdPosition(lcd, 0, 0);
-		lcdPrintf(lcd, "%.2f", distance);
+        serialPrintf(fd, "#Bbff040,040");
+        distance = averageDistance();
+        lcdPosition(lcd, 0, 0);
+        lcdPrintf(lcd, "%.2f", distance);
+    }
+    while(distance > 20);
 
-	}while(distance > 20);
-	
-	serialPrintf(fd, "#Hb");
-	
-	return 0 ;
+    serialPrintf(fd, "#Hb");
+
+    return 0 ;
 }
